35b6589d11e08ad9a73a6a637648d8b70e1ee92b
solrobson <solrobson@live.com>
2015-06-28 22:42:42 -0400

    Added ProductListController
    Added the product list controller, which is used to filter the products.

---
+++ angularjs/controllers/productListControllers.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../scripts/angular.js" />
+
+angular.module("sportsStore")
+    .controller("productListCtrl", function ($scope, $filter) {
+        var selectedCategory = null;
+
+        $scope.selectCategory = function (newCategory) {
+            selectedCategory = newCategory;
+        }
+
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory === null ||
+                product.category === selectedCategory;
+        }
+    });
