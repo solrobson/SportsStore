5eab8f709729ff058823867164f1d46a2a0a7822
solrobson <solrobson@live.com>
2015-06-28 22:29:39 -0400

    Adding Controller and Filter
    Adding the SportsStore controller and a custom filter

---
+++ angularjs/controllers/sportsStore.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../scripts/angular.js" />
+
+angular.module("sportsStore")
+    .controller("sportsStoreCtrl", function ($scope) {
+        $scope.data = {
+            products: [
+                {
+                    name: "Product #1", description: "A Product",
+                    category: "Category #1", price: 100
+                },
+                {
+                    name: "Product #2", description: "A Product",
+                    category: "Category #1", price: 100
+                },
+                {
+                    name: "Product #3", description: "A Product",
+                    category: "Category #2", price: 100
+                },
+                {
+                    name: "Product #4", description: "A Product",
+                    category: "Category #3", price: 100
+                }
+            ]
+        }
+    })
---
+++ angularjs/filters/customerFilters.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../scripts/angular.js" />
+
+angular.module("customFilters", [])
+.filter("unique", function () {
+    return function (data, propertyName) {
+        if (angular.isArray(data) && angular.isString(propertyName)) {
+            var result = [];
+            var keys = {};
+            for (var i = 0; i < data.length; i++) {
+                var val = data[i][propertyName];
+                if (angular.isUndefined(keys[val])) {
+                    keys[val] = true;
+                    result.push(val);
+                }
+            }
+            return result;
+        } else {
+            return data;
+        }
+    }
+});
