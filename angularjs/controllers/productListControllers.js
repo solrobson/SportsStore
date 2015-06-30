f3deb425e45f48c03eef3bde78c324085fd74e83
solrobson <solrobson@live.com>
2015-06-29 20:25:54 -0400

    Finished Chapter 7
    Adding the files that wrap up chapter 7

--- angularjs/controllers/productListControllers.js
+++ angularjs/controllers/productListControllers.js
@@ -1,15 +1,35 @@
 ﻿/// <reference path="../scripts/angular.js" />
 
 angular.module("sportsStore")
-    .controller("productListCtrl", function ($scope, $filter) {
+    .constant("productListActiveClass", "btn-primary")
+    .constant("productListPageCount", 3)
+    .controller("productListCtrl", function ($scope, $filter,
+        productListActiveClass, productListPageCount) {
+
         var selectedCategory = null;
+
+        $scope.selectedPage = 1;
+        $scope.pageSize = productListPageCount;
 
         $scope.selectCategory = function (newCategory) {
             selectedCategory = newCategory;
+            $scope.selectedPage = 1;
+        }
+
+        $scope.selectedPage = function (newPage) {
+            $scope.selectedPage = newPage;
         }
 
         $scope.categoryFilterFn = function (product) {
-            return selectedCategory === null ||
-                product.category === selectedCategory;
+            return selectedCategory == null ||
+                product.category == selectedCategory;
+        }
+
+        $scope.getCategoryClass = function (category) {
+            return selectedCategory == category ? productListActiveClass : "";
+        }
+
+        $scope.getPageCLass = function (page) {
+            return $scope.selectedPage == page ? productListActiveClass : "";
         }
     });
--- angularjs/filters/customerFilters.js
+++ angularjs/filters/customerFilters.js
@@ -18,4 +18,31 @@ angular.module("customFilters", [])
             return data;
         }
     }
+})
+.filter("range", function ($filter) {
+    return function (data, page, size) {
+        if (angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)) {
+            var start_index = (page - 1) * size;
+            if (Data.length < start_index) {
+                retunr [];
+            } else {
+                return $filter("limitTo")(data.splice(start_indx), size);
+            }
+        } else {
+            return data;
+        }
+    }
+})
+.filter("pageCount", function () {
+    return function(data, size) {
+        if(angular.isArray(data)) {
+            var result = [];
+            for (var i = 0; i < Math.ceil(data.length / size); i++) {
+                result.push(i);
+            }
+            return result;
+        } else {
+            return data;
+        }
+    }
 });
